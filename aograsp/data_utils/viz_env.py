2ce77040a9027a83345ccbeed33767a40e8de9ac
Claire Chen <clairech@stanford.edu>
2024-04-01 11:06:11 -0700

    Dataset readme and utils (#4)

---
+++ aograsp/data_utils/viz_env.py
@@ -0,0 +1,193 @@
+import pybullet as p
+import os
+import sys
+import numpy as np
+import pandas
+from matplotlib.colors import LinearSegmentedColormap
+from scipy.spatial.transform import Rotation as R
+
+from aograsp.data_utils.object import Object
+import aograsp.rotation_utils as r_utils
+from aograsp.mesh_utils import create_gripper
+
+PARTNET_MOBILITY_PATH = "aograsp_instances"
+
+
+class VizEnv:
+    def __init__(
+        self,
+        _p,
+        init_state,
+        save_dir=None,
+        draw_gripper=True,
+        draw_point=False,
+    ):
+        # Set sim parameters
+        self._p = _p
+        self.save_dir = save_dir
+        self._time_step = 0.01
+        self._set_pybullet_sim_params()
+
+        self.object = self.init_obj(init_state)
+
+        self.draw_gripper = draw_gripper
+        self.draw_point = draw_point
+
+        # IDs for debug lines
+        self.cmap = LinearSegmentedColormap.from_list(
+            "", ["crimson", "gold", "limegreen"]
+        )
+
+        # Gripper viz init
+        g_opening = 0.07
+        gripper = create_gripper("panda")
+        gripper_control_points = gripper.get_control_point_tensor(
+            1, False, convex_hull=False
+        ).squeeze()
+        mid_point = 0.5 * (gripper_control_points[1, :] + gripper_control_points[2, :])
+        grasp_line_plot = np.array(
+            [
+                np.zeros((3,)),
+                mid_point,
+                gripper_control_points[1],
+                gripper_control_points[3],
+                gripper_control_points[1],
+                gripper_control_points[2],
+                gripper_control_points[4],
+            ]
+        )
+        self.gripper_control_points_closed = grasp_line_plot.copy()
+        self.gripper_control_points_closed[2:, 0] = (
+            np.sign(grasp_line_plot[2:, 0]) * g_opening / 2
+        )
+
+    def init_obj(self, object_state):
+        """Add object from grasp dataset given init_state dict"""
+
+        object_path = os.path.join(
+            PARTNET_MOBILITY_PATH,
+            object_state["ins_id"],
+        )
+
+        obj = Object(
+            self._p,
+            object_path,
+            scaling=object_state["scaling"],
+        )
+        obj.reset(
+            object_state["qpos"],
+            trans=object_state["trans"],
+            quat=object_state["quat"],
+        )
+
+        # transformation from object to world for object pose in dataset
+        self.data_H_obj_to_world = obj.H_link_to_world(obj.base_link_id)
+
+        return obj
+
+    def reset(self, grasp_data_dict, data_name=""):
+        """Environment reset called at the beginning of an episode."""
+
+        # Visualize grasps
+        if grasp_data_dict is not None:
+            self.viz_grasps(grasp_data_dict)
+
+            self._p.addUserDebugText(
+                data_name,
+                [-0.5, 0, 0.5],
+                textColorRGB=[0, 0, 0],
+                textSize=1.5,
+            )
+
+        self._p.stepSimulation()
+
+    def viz_grasps(self, grasp_data_dict):
+        for i, grasp_data in grasp_data_dict.items():
+            self.viz_grasp_from_data(grasp_data)
+
+    def viz_grasp_from_data(
+        self,
+        grasp_data,
+    ):
+        """Get grasp info in object frame"""
+
+        data_H_world_to_obj = r_utils.get_H_inv(self.data_H_obj_to_world)
+
+        target_pos_wf = (
+            self.data_H_obj_to_world @ np.append(grasp_data["target_pos"], 1)
+        )[:3]
+
+        print("Drawing grasp #", grasp_data["n_proposal"])
+        color = self.cmap(grasp_data["prob"])[:3]
+        pt_color = self.cmap(grasp_data["prob"])[:3]
+
+        if self.draw_point:
+            # Draw point in world frame
+            self._p.addUserDebugPoints(
+                [target_pos_wf],
+                pointColorsRGB=[pt_color],
+                pointSize=10.0,
+            )
+
+        if self.draw_gripper:
+            # Compute gripper viz pts in object frame
+            g = np.zeros((4, 4))
+            rot = r_utils.get_matrix_from_quat(grasp_data["start_ori"])
+            g[:3, :3] = rot
+            g[:3, 3] = grasp_data["target_pos"].T
+            g[3, 3] = 1
+            r = R.from_euler("z", 90, degrees=True)
+            gripper_control_points = self.gripper_control_points_closed.copy()
+            gripper_control_points = r.apply(gripper_control_points)
+            z = gripper_control_points[-1, -1]
+            gripper_control_points[:, -1] -= z
+            gripper_control_points[[1], -1] -= 0.02
+            pts = np.matmul(gripper_control_points, g[:3, :3].T)
+            pts += np.expand_dims(g[:3, 3], 0)
+
+            lines = [[0, 1], [2, 3], [1, 4], [1, 5], [5, 6]]
+
+            # pts, lines = grasp_data["grasp"]
+            for line in lines:
+                o, d = line
+                # Compute gripper viz pts in world frame
+                o_wf = (self.data_H_obj_to_world @ np.append(pts[o], 1))[:3]
+                e_wf = (self.data_H_obj_to_world @ np.append(pts[d], 1))[:3]
+                self._p.addUserDebugLine(
+                    o_wf,
+                    e_wf,
+                    lineColorRGB=color,
+                    lineWidth=3.0,
+                )
+
+    def step(self):
+        self._p.stepSimulation()
+
+    def _set_pybullet_sim_params(self):
+        """Set pybullet simulation parameters"""
+
+        self._p.resetSimulation()
+        self._p.setTimeStep(self._time_step)
+        self._p.setPhysicsEngineParameter(enableConeFriction=1)
+        self._p.setPhysicsEngineParameter(contactBreakingThreshold=0.001)
+        self._p.setPhysicsEngineParameter(allowedCcdPenetration=0.0)
+        self._p.setPhysicsEngineParameter(numSolverIterations=200)
+        self._p.setPhysicsEngineParameter(numSubSteps=5)
+        self._p.setPhysicsEngineParameter(
+            constraintSolverType=self._p.CONSTRAINT_SOLVER_LCP_DANTZIG,
+            globalCFM=0.000001,
+        )
+        self._p.setPhysicsEngineParameter(enableFileCaching=0)
+        self._p.setGravity(0, 0, 0)  # Set 0 gravity to prevent joints from falling
+        self._p.setRealTimeSimulation(0)
+
+        # Debug visualizer camera params
+        self._p.resetDebugVisualizerCamera(
+            cameraDistance=1.0,
+            cameraYaw=-40,
+            cameraPitch=-30,
+            cameraTargetPosition=(0, 0, 0),
+        )
+        # Turn off extra menus for a cleaner pybullet UI.
+        self._p.configureDebugVisualizer(self._p.COV_ENABLE_GUI, 0)
+        self._p.configureDebugVisualizer(self._p.COV_ENABLE_SHADOWS, 0)
--- aograsp/viz_utils.py
+++ aograsp/viz_utils.py
@@ -9,6 +9,9 @@ import matplotlib
 import matplotlib.pyplot as plt
 import matplotlib.cm
 import open3d as o3d
+from scipy.spatial.transform import Rotation
+
+import aograsp.mesh_utils as mesh_utils
 
 
 def get_o3d_pts(pts):
@@ -119,3 +122,152 @@ def viz_histogram(
     else:
         plt.savefig(save_path)
     plt.close()
+
+
+def get_eef_line_set_for_o3d_viz(eef_pos_list, eef_quat_list, highlight_top_k=None):
+    # Get base gripper points
+    g_opening = 0.07
+    gripper = mesh_utils.create_gripper("panda")
+    gripper_control_points = gripper.get_control_point_tensor(
+        1, False, convex_hull=False
+    ).squeeze()
+    mid_point = 0.5 * (gripper_control_points[1, :] + gripper_control_points[2, :])
+    grasp_line_plot = np.array(
+        [
+            np.zeros((3,)),
+            mid_point,
+            gripper_control_points[1],
+            gripper_control_points[3],
+            gripper_control_points[1],
+            gripper_control_points[2],
+            gripper_control_points[4],
+        ]
+    )
+    gripper_control_points_base = grasp_line_plot.copy()
+    gripper_control_points_base[2:, 0] = np.sign(grasp_line_plot[2:, 0]) * g_opening / 2
+    # Need to rotate base points, our gripper frame is different
+    # ContactGraspNet
+    r = Rotation.from_euler("z", 90, degrees=True)
+    gripper_control_points_base = r.apply(gripper_control_points_base)
+
+    # Compute gripper viz pts based on eef_pos and eef_quat
+    line_set_list = []
+    for i in range(len(eef_pos_list)):
+        eef_pos = eef_pos_list[i]
+        eef_quat = eef_quat_list[i]
+
+        gripper_control_points = gripper_control_points_base.copy()
+        g = np.zeros((4, 4))
+        rot = Rotation.from_quat(eef_quat).as_matrix()
+        g[:3, :3] = rot
+        g[:3, 3] = eef_pos.T
+        g[3, 3] = 1
+        z = gripper_control_points[-1, -1]
+        gripper_control_points[:, -1] -= z
+        gripper_control_points[[1], -1] -= 0.02
+        pts = np.matmul(gripper_control_points, g[:3, :3].T)
+        pts += np.expand_dims(g[:3, 3], 0)
+
+        lines = [[0, 1], [2, 3], [1, 4], [1, 5], [5, 6]]
+        if highlight_top_k is not None:
+            if i < highlight_top_k:
+                # Draw grasp in green
+                colors = [[0, 1, 0] for i in range(len(lines))]
+            else:
+                colors = [[0, 0, 0] for i in range(len(lines))]
+        else:
+            colors = [[0, 0, 0] for i in range(len(lines))]
+
+        line_set = o3d.geometry.LineSet(
+            points=o3d.utility.Vector3dVector(pts),
+            lines=o3d.utility.Vector2iVector(lines),
+        )
+        line_set.colors = o3d.utility.Vector3dVector(colors)
+        line_set_list.append(line_set)
+
+    return line_set_list
+
+
+def viz_pts_and_eef_o3d(
+    pts_pcd,
+    eef_pos_list,
+    eef_quat_list,
+    heatmap_labels=None,
+    save_path=None,
+    frame="world",
+    draw_frame=False,
+    highlight_top_k=None,
+    pcd_rgb=None,
+):
+    """
+    Plot eef in o3d visualization, with point cloud, at positions and
+    orientations specified in eef_pos_list and eef_quat_list
+    pts_pcd, eef_pos_list, and eef_quat_list need to be in same frame
+    """
+
+    pcd = get_o3d_pts(pts_pcd)
+    if pcd_rgb is not None:
+        pcd.colors = o3d.utility.Vector3dVector(pcd_rgb)
+    else:
+        if heatmap_labels is not None:
+            # Scale heatmap for visualization
+            heatmap_labels = scale_to_0_1(heatmap_labels)
+
+            cmap = matplotlib.cm.get_cmap("RdYlGn")
+            colors = cmap(np.squeeze(heatmap_labels))[:, :3]
+            pcd.colors = o3d.utility.Vector3dVector(colors)
+
+    # Get line_set for drawing eef in o3d
+    line_set_list = get_eef_line_set_for_o3d_viz(
+        eef_pos_list,
+        eef_quat_list,
+        highlight_top_k=highlight_top_k,
+    )
+
+    vis = o3d.visualization.Visualizer()
+    vis.create_window()
+
+    vis.add_geometry(pcd)
+
+    for line_set in line_set_list:
+        vis.add_geometry(line_set)
+
+    # Draw ref frame
+    if draw_frame:
+        mesh_frame = o3d.geometry.TriangleMesh.create_coordinate_frame(
+            size=0.1, origin=[0, 0, 0]
+        )
+        vis.add_geometry(mesh_frame)
+
+    # Move camera
+    if frame == "camera":
+        # If visualizing in camera frame, view pcd from scene view
+        ctr = vis.get_view_control()
+        param = ctr.convert_to_pinhole_camera_parameters()
+        fov = ctr.get_field_of_view()
+        H = np.eye(4)
+        param.extrinsic = H
+        ctr.convert_from_pinhole_camera_parameters(param)
+    else:
+        # If world frame, place camera accordingly to face object front
+        ctr = vis.get_view_control()
+        param = ctr.convert_to_pinhole_camera_parameters()
+        fov = ctr.get_field_of_view()
+        H = np.eye(4)
+        H[2, -1] = 1
+        R = Rotation.from_euler("XYZ", [90, 0, 90], degrees=True).as_matrix()
+        H[:3, :3] = R
+        param.extrinsic = H
+        ctr.convert_from_pinhole_camera_parameters(param)
+
+    vis.poll_events()
+    vis.update_renderer()
+
+    if save_path is None:
+        vis.run()
+    else:
+        vis.capture_screen_image(
+            save_path,
+            do_render=True,
+        )
+    vis.destroy_window()
