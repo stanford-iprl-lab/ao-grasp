60cf3aa6c5fe5d1010ac8311a79cf486bbf24eee
ClaireLC <clairech@stanford.edu>
2024-04-01 14:20:21 -0700

    Function to transform pts

--- aograsp/rotation_utils.py
+++ aograsp/rotation_utils.py
@@ -1,5 +1,6 @@
 import numpy as np
 from scipy.spatial.transform import Rotation
+import os
 
 
 def get_H(R, p):
@@ -31,3 +32,22 @@ def get_matrix_from_ori(ori):
 
 def get_matrix_from_quat(quat):
     return Rotation.from_quat(quat).as_matrix()
+
+
+def transform_pts(pts_in, H):
+    """Transform pts ([N, 3] or [3,]) by H"""
+    # Check pts dim and reshape
+    if pts_in.ndim == 1:
+        pts = np.expand_dims(pts_in, axis=0)
+    else:
+        pts = pts_in
+
+    pts_new = np.concatenate([pts, np.ones_like(pts[:, :1])], axis=-1)  # [N, 4]
+    pts_new = np.matmul(H, pts_new.T).T[
+        :, :3
+    ]  # [4, 4], [4, N] -> [4, N] -> [N, 4] -> [N, 3]
+
+    if pts_in.ndim == 1:
+        return np.squeeze(pts_new)
+    else:
+        return pts_new
